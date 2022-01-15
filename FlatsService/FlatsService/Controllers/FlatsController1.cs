c8b14f6c0f575901f7903d52658364c9cf0a1364
Belka2000 <her2025@mail.ru>
2022-01-15 19:12:16 +0300

    git init

---
+++ FlatsService/FlatsService/Controllers/FlatsController1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatsService.Controllers
+{
+    public class FlatsController1 : Controller
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
